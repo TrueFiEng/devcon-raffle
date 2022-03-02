54f69744eddddfc5e74fd4cb414fc69153227a7b
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-02 17:24:21 +0100

    Add CI lint and test jobs

--- packages/contracts/test/contracts/Example.test.ts
+++ packages/contracts/test/contracts/Example.test.ts
@@ -1,16 +1,16 @@
-import { setupFixtureLoader } from "../setup";
-import { expect } from "chai";
-import { exampleFixture } from "./fixture";
-import { BigNumber } from "ethers";
+import { setupFixtureLoader } from '../setup'
+import { expect } from 'chai'
+import { exampleFixture } from './fixture'
+import { BigNumber } from 'ethers'
 
-describe("Example", () => {
-  const loadFixture = setupFixtureLoader();
+describe('Example', () => {
+  const loadFixture = setupFixtureLoader()
 
-  it("add", async () => {
-    const { example } = await loadFixture(exampleFixture);
+  it('add', async () => {
+    const { example } = await loadFixture(exampleFixture)
 
-    await example.add(BigNumber.from(5));
+    await example.add(BigNumber.from(5))
 
-    expect(await example.a()).to.eq(BigNumber.from(10));
-  });
-});
+    expect(await example.a()).to.eq(BigNumber.from(10))
+  })
+})
--- packages/contracts/test/contracts/fixture.ts
+++ packages/contracts/test/contracts/fixture.ts
@@ -1,5 +1,5 @@
 import { Example__factory } from 'build/types'
-import {Wallet} from "ethers";
+import { Wallet } from 'ethers'
 
 export async function exampleFixture ([owner]: Wallet[]) {
   const example = await new Example__factory(owner).deploy()
