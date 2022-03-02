dd57ec12509a5c8c4fbb68d793c0e826696277c7
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-02 15:57:44 +0100

    Add example contract

---
+++ packages/contracts/test/contracts/fixture.ts
@@ -0,0 +1,8 @@
+import { Example__factory } from 'build/types'
+import {Wallet} from "ethers";
+
+export async function exampleFixture ([owner]: Wallet[]) {
+  const example = await new Example__factory(owner).deploy()
+
+  return { example }
+}
