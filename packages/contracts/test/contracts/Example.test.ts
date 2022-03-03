89abdc6b04991afa1f1c7ae22a3a323b4cb13e1e
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2022-03-03 12:34:13 +0100

    📦 Setup project (#1)
    * Setup package.json
    
    * Setup eslint
    
    * Add contracts dependencies
    
    * Add example contract
    
    * Remove unnecessary packages
    
    * Add CI lint and test jobs
    
    * Add missing step in CI
    
    * Add licenses
    
    * Add cache to jobs
    
    * Remove dependencies section from package.json
    
    * Remove console logs
    
    * Bump solidity version to 0.8.10
    
    * Remove space before function parentheses
    
    * Update hardhat.config.ts
    
    * Move fixture to separate directory

---
+++ packages/contracts/test/contracts/Example.test.ts
@@ -0,0 +1,16 @@
+import { setupFixtureLoader } from '../setup'
+import { expect } from 'chai'
+import { exampleFixture } from './fixtures/fixture'
+import { BigNumber } from 'ethers'
+
+describe('Example', () => {
+  const loadFixture = setupFixtureLoader()
+
+  it('add', async() => {
+    const { example } = await loadFixture(exampleFixture)
+
+    await example.add(BigNumber.from(5))
+
+    expect(await example.a()).to.eq(BigNumber.from(10))
+  })
+})
