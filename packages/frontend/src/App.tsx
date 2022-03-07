99c0fa1be8142d2cf6ebcfafdcf687d484a48f07
p-sad <sadlik512@gmail.com>
2022-03-07 13:18:33 +0100

    Add vite

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
