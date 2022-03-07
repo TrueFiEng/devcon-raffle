5ffb85be4c56181838ddc7c369fadfc35e7b7559
Maria Rushkova <maria@ethworks.io>
2022-03-07 17:13:18 +0100

    Add color variables

--- packages/frontend/src/pages/Hello.tsx
+++ packages/frontend/src/pages/Hello.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Colors } from '../styles/colors'
 
 export const Hello = () => <HelloHeader>Hello World!</HelloHeader>
 
 const HelloHeader = styled.h1`
   font-weight: 700;
+  color: ${Colors.Waterloo};
 `
