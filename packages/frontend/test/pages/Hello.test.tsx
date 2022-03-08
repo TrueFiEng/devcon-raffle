883cc7eee6f605b1653919040710a7909526c47e
Maria Rushkova <maria@ethworks.io>
2022-03-08 15:11:45 +0100

    Update test example

--- packages/frontend/test/pages/Hello.test.tsx
+++ packages/frontend/test/pages/Hello.test.tsx
@@ -1,10 +1,9 @@
 import { screen, render } from '@testing-library/react'
-import React from 'react'
-import { Hello } from 'src/pages/Hello'
+import { Header } from 'src/components/Header'
 
 it('something', async () => {
   renderHello()
   expect(await screen.findByText('Hello World!')).toBeDefined()
 })
 
-const renderHello = () => render(<Hello />)
+const renderHello = () => render(<Header />)
