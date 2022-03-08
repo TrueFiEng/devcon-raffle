5b7cdebbf8dbd504ff6c9360c56e267f3e5404ca
Piotr Sadlik <sadlik512@gmail.com>
2022-03-08 10:12:22 +0100

    🦑 Setup frontend package (#5)
    * Add vite
    
    * Add linter
    
    * Add useDApp and styled-components
    
    * Add Jest
    
    * Skip library check
    
    * Extract Jest config
    
    * Set typescript version to ~4.5.4
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Hello } from './pages/Hello'
+import { Providers } from './providers/Providers'
+
+function App() {
+  return (
+    <Providers>
+      <Hello />
+    </Providers>
+  )
+}
+
+export default App
---
+++ packages/frontend/src/pages/Hello.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Hello = () => <HelloHeader>Hello World!</HelloHeader>
+
+const HelloHeader = styled.h1`
+  font-weight: 700;
+`
