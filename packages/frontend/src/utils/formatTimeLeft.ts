5980a51d84c655ba7d673fd93b9841d2f872eb0d
p-sad <sadlik512@gmail.com>
2022-03-08 15:32:13 +0100

    Add remaining time formatter

---
+++ packages/frontend/src/utils/formatTimeLeft.ts
@@ -0,0 +1,24 @@
+import { BigNumber } from '@ethersproject/bignumber'
+
+const MillisInMinute = 1000 * 60
+const SecondsInMinute = 60
+const MinutesInHour = 60
+const MinutesInDay = 24 * 60
+
+export function formatTimeLeft(dateSeconds: BigNumber, now = Date.now()) {
+  const dateMinutes = dateSeconds.div(SecondsInMinute)
+  const nowMinutes = Math.floor(now / MillisInMinute)
+
+  const difference = dateMinutes.gt(nowMinutes) ? dateMinutes.sub(nowMinutes) : BigNumber.from(0)
+
+  const days = difference.div(MinutesInDay).toString()
+  const hours = padZero(difference.mod(MinutesInDay).div(MinutesInHour))
+  const minutes = padZero(difference.mod(MinutesInHour))
+
+  return `${days}d ${hours}h ${minutes}m`
+}
+
+function padZero(number: BigNumber) {
+  const numberString = number.toString()
+  return numberString.length === 1 ? '0' + numberString : numberString
+}
