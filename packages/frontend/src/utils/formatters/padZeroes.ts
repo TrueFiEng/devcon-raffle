38e9557543555563b03a2a8849a4a201302f1242
p-sad <sadlik512@gmail.com>
2022-03-09 12:52:23 +0100

    Use padStart in padZeroes

--- packages/frontend/src/utils/formatters/padZeroes.ts
+++ packages/frontend/src/utils/formatters/padZeroes.ts
@@ -1,7 +1,5 @@
 import { BigNumber } from '@ethersproject/bignumber'
 
 export function padZeroes(number: BigNumber | number, length = 2) {
-  const numberString = number.toString()
-  const missingLength = length - numberString.length
-  return missingLength > 0 ? '0'.repeat(missingLength) + numberString : numberString
+  return number.toString().padStart(length, '0')
 }
