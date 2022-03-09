23cba5f79e6797aa330844e79892178de88b8824
Maria Rushkova <maria@ethworks.io>
2022-03-09 16:21:57 +0100

    Add form styles

---
+++ packages/frontend/src/components/Form/Form.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  display: grid;
+  grid-row-gap: 16px;
+  width: 100%;
+  max-width: 460px;
+  overflow: hidden;
+`
+
+export const FormRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
