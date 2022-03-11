b748e8097667eaf5149957642e7e9a70c87ea504
Michał Sieczkowski <michal@ethworks.io>
2022-03-11 17:23:59 +0100

    Remove bids

--- packages/frontend/src/data/bids.ts
+++ packages/frontend/src/data/bids.ts
@@ -1,12 +1,3 @@
 import { Bid } from 'src/models/Bid'
 
-export const bidList: Bid[] = [
-  {
-    address: '0x6b175474e89094c44da98b954eedeac495271d0f',
-    bid: 3,
-  },
-  {
-    address: '0x6b175474e89094c44da98b954eedeac4f5551d0f',
-    bid: 3.5,
-  },
-]
+export const bidList: Bid[] = []
