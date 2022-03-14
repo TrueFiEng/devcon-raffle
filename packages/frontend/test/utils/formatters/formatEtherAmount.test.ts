c70e306a65d1d3614a004b3d74dc2a0e391f3a41
Michał Sieczkowski <michal@ethworks.io>
2022-03-14 13:42:26 +0100

    Add one more test for formatEtherAmount

--- packages/frontend/test/utils/formatters/formatEtherAmount.test.ts
+++ packages/frontend/test/utils/formatters/formatEtherAmount.test.ts
@@ -12,6 +12,10 @@ describe('formatEtherAmount', () => {
     expect(formatEtherAmount(parseEther('1923.123456789'))).toBe('1923.123457')
   })
 
+  it('Handles numbers smaller than 1', () => {
+    expect(formatEtherAmount(parseEther('0.0003819'))).toBe('0.000382')
+  })
+
   it('Handles numbers with fewer than 6 decimal places', () => {
     expect(formatEtherAmount(parseEther('1923.12'))).toBe('1923.12')
   })
