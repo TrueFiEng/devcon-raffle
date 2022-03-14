a1fc779d8926663f2eae9dfc87eaf4b8e8da61d0
p-sad <sadlik512@gmail.com>
2022-03-14 16:15:45 +0100

    Align items in top bar

--- packages/frontend/src/components/TopBar/TopBar.tsx
+++ packages/frontend/src/components/TopBar/TopBar.tsx
@@ -7,27 +7,19 @@ import { AccountButton } from './AccountButton'
 export const TopBar = () => {
   return (
     <TopBarContainer>
-      <Wrapper>
-        <Logo />
-        <AccountButton />
-      </Wrapper>
+      <Logo />
+      <AccountButton />
     </TopBarContainer>
   )
 }
 
 const TopBarContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 56px;
-  width: 100%;
-  background: ${Colors.White};
-`
-
-const Wrapper = styled.div`
-  display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding: 0 68px;
+  align-items: center;
+  min-height: 56px;
   width: 100%;
+  padding: 0 68px;
+  background: ${Colors.White};
 `
