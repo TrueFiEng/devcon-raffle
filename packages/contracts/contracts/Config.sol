1336454eadec9f009287fa52f47e5290cdc4de02
Michał Sieczkowski <michal@ethworks.io>
2022-03-16 14:33:25 +0100

    🎛️ Validate constructor parameters (#30)
    * Validate auction and raffle winners counts
    
    * Add validations for times

--- packages/contracts/contracts/Config.sol
+++ packages/contracts/contracts/Config.sol
@@ -23,8 +23,24 @@ contract Config {
         uint256 minBidIncrement_
     ) {
         require(
+            auctionWinnersCount_ > 0,
+            "Config: auction winners count must be greater than 0"
+        );
+        require(
+            raffleWinnersCount_ > 0,
+            "Config: raffle winners count must be greater than 0"
+        );
+        require(
             raffleWinnersCount_ % 8 == 0,
-            "Devcon6: raffle winners count must be divisible by 8"
+            "Config: raffle winners count must be divisible by 8"
+        );
+        require(
+            biddingStartTime_ < biddingEndTime_,
+            "Config: bidding start time must be before bidding end time"
+        );
+        require(
+            biddingEndTime_ < claimingEndTime_,
+            "Config: bidding end time must be before claiming end time"
         );
 
         _biddingStartTime = biddingStartTime_;
---
+++ packages/contracts/test/contracts/Config.behaviour.test.ts
@@ -0,0 +1,44 @@
+import { setupFixtureLoader } from '../setup'
+import { configuredDevcon6Fixture } from 'fixtures/devcon6Fixture'
+import { expect } from 'chai'
+
+describe('Config', function () {
+  const loadFixture = setupFixtureLoader()
+
+  describe('constructor', function () {
+    it('reverts for zero auction winners count', async function () {
+      await expect(loadFixture(configuredDevcon6Fixture({ auctionWinnersCount: 0 })))
+        .to.be.revertedWith('Config: auction winners count must be greater than 0')
+    })
+
+    it('reverts for zero raffle winners count', async function () {
+      await expect(loadFixture(configuredDevcon6Fixture({ raffleWinnersCount: 0 })))
+        .to.be.revertedWith('Config: raffle winners count must be greater than 0')
+    })
+
+    it('reverts for raffle winners count non-divisible by 8', async function () {
+      await expect(loadFixture(configuredDevcon6Fixture({ raffleWinnersCount: 7 })))
+        .to.be.revertedWith('Config: raffle winners count must be divisible by 8')
+    })
+
+    it('reverts for bidding start time after bidding end time', async function () {
+      await expect(loadFixture(
+        configuredDevcon6Fixture({
+          biddingStartTime: 200,
+          biddingEndTime: 100,
+          claimingEndTime: 300,
+        }),
+      )).to.be.revertedWith('Config: bidding start time must be before bidding end time')
+    })
+
+    it('reverts for bidding start time after bidding end time', async function () {
+      await expect(loadFixture(
+        configuredDevcon6Fixture({
+          biddingStartTime: 100,
+          biddingEndTime: 300,
+          claimingEndTime: 200,
+        }),
+      )).to.be.revertedWith('Config: bidding end time must be before claiming end time')
+    })
+  })
+})
