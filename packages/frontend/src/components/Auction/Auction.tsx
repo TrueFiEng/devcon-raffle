cd0f5fc5a5f9b1cd4d14cdda4a732d296721d4f7
Michał Sieczkowski <michal@ethworks.io>
2022-03-17 14:41:33 +0100

    Remove AuctionAction component

--- packages/frontend/src/components/Auction/Auction.tsx
+++ packages/frontend/src/components/Auction/Auction.tsx
@@ -1,4 +1,3 @@
-import { AuctionAction } from 'src/components/Auction/AuctionAction'
 import { PlaceBidFlow } from 'src/components/Bid/PlaceBid/PlaceBidFlow'
 import { BidsListSection } from 'src/components/BidsList/BidsList'
 import { bids } from 'src/data/bids'
@@ -8,9 +7,9 @@ import styled from 'styled-components'
 export const Auction = () => {
   return (
     <Wrapper>
-      <AuctionAction>
+      <ActionSection>
         <PlaceBidFlow />
-      </AuctionAction>
+      </ActionSection>
       <BidsListSection bids={bids} />
     </Wrapper>
   )
@@ -22,4 +21,13 @@ const Wrapper = styled.div`
   width: 670px;
   padding: 0 115px;
   background: ${Colors.GreyLight};
+`
+const ActionSection = styled.div`
+  display: flex;
+  margin-left: -170px;
+  width: 724px;
+  height: 450px;
+  background-color: ${Colors.Blue};
+  position: relative;
+  z-index: 100;
 `
