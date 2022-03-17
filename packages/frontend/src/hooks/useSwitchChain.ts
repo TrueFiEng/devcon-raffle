89e358b964a76bff86f4b9a55654ae28de9d1e81
Michał Sieczkowski <michal@ethworks.io>
2022-03-17 15:24:07 +0100

    Extract new functions in useSwitchChain hook

--- packages/frontend/src/hooks/useSwitchChain.ts
+++ packages/frontend/src/hooks/useSwitchChain.ts
@@ -1,28 +1,40 @@
 import { BigNumber } from '@ethersproject/bignumber'
+import { JsonRpcProvider } from '@ethersproject/providers'
 import { ChainId, useEthers } from '@usedapp/core'
 import { useCallback } from 'react'
 
 const arbitrumChainId = BigNumber.from(ChainId.Arbitrum).toHexString()
+const errChainNotAddedYet = 4902
 
 export function useSwitchChain() {
   const { library } = useEthers()
   return useCallback(async () => {
-    try {
-      await library?.send('wallet_switchEthereumChain', [{ chainId: arbitrumChainId }])
-    } catch (switchChainError: any) {
-      if (switchChainError.code === 4902) {
-        try {
-          await library?.send('wallet_addEthereumChain', [
-            {
-              chainId: arbitrumChainId,
-              chainName: 'Arbitrum',
-              rpcUrls: ['https://arb1.arbitrum.io/rpc'],
-            },
-          ])
-        } catch (error: any) {
-          console.error(error)
-        }
-      }
+    if (library !== undefined) {
+      await switchOrAddChain(library)
     }
   }, [library])
+}
+
+async function switchOrAddChain(library: JsonRpcProvider) {
+  try {
+    await library.send('wallet_switchEthereumChain', [{ chainId: arbitrumChainId }])
+  } catch (switchChainError: any) {
+    if (switchChainError.code === errChainNotAddedYet) {
+      await addChain(library)
+    }
+  }
+}
+
+async function addChain(library: JsonRpcProvider) {
+  try {
+    await library.send('wallet_addEthereumChain', [
+      {
+        chainId: arbitrumChainId,
+        chainName: 'Arbitrum',
+        rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+      },
+    ])
+  } catch (error: any) {
+    console.error(error)
+  }
 }
