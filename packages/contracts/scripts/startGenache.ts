dd6a9f2db2fbbe7a204542ab9fc795b67b082a2f
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-17 16:41:48 +0100

    Split logging accounts

--- packages/contracts/scripts/startGenache.ts
+++ packages/contracts/scripts/startGenache.ts
@@ -15,8 +15,17 @@ export function startGanache(): Web3Provider {
   console.log(`Ganache started at: localhost:${PORT}`)
   console.log('Available accounts')
   console.log('Address\t\t\t\t\t   Private key\t\t\t\t\t\t\t      Balance')
-  console.log(defaultAccounts.map(
-    ({ balance, secretKey }) => `${new Wallet(secretKey).address} ${secretKey} ${utils.formatEther(balance).toString()}${constants.EtherSymbol}`).join('\n')
-  )
+  logAccounts()
   return new providers.Web3Provider(server.provider)
+}
+
+function logAccounts() {
+  console.log(defaultAccounts.map(
+    ({ balance, secretKey }) =>
+      `${new Wallet(secretKey).address} ${secretKey} ${formatBalance(balance)}`).join('\n')
+  )
+}
+
+function formatBalance(balance: string): string {
+  return `${utils.formatEther(balance).toString()}${constants.EtherSymbol}`
 }
