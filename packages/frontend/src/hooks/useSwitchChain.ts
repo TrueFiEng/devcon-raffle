335fefad900ce8b4d43993663cf0ecb49d00cdd4
Michał Sieczkowski <michal@ethworks.io>
2022-03-17 15:41:41 +0100

    Extract consts with MetaMask request parameters

--- packages/frontend/src/hooks/useSwitchChain.ts
+++ packages/frontend/src/hooks/useSwitchChain.ts
@@ -3,8 +3,30 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 import { ChainId, useEthers } from '@usedapp/core'
 import { useCallback } from 'react'
 
+interface AddEthereumChainParameter {
+  chainId: string
+  chainName: string
+  rpcUrls: string[]
+  blockExplorerUrls?: string[]
+}
+
+interface SwitchEthereumChainParameter {
+  chainId: string // A 0x-prefixed hexadecimal string
+}
+
 const arbitrumChainId = BigNumber.from(ChainId.Arbitrum).toHexString()
 const errChainNotAddedYet = 4902
+
+const addArbitrumChain: AddEthereumChainParameter = {
+  chainId: arbitrumChainId,
+  chainName: 'Arbitrum One',
+  rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+  blockExplorerUrls: ['https://arbiscan.io'],
+}
+
+const switchArbitrumChain: SwitchEthereumChainParameter = {
+  chainId: arbitrumChainId,
+}
 
 export function useSwitchChain() {
   const { library } = useEthers()
@@ -17,7 +39,7 @@ export function useSwitchChain() {
 
 async function switchOrAddChain(library: JsonRpcProvider) {
   try {
-    await library.send('wallet_switchEthereumChain', [{ chainId: arbitrumChainId }])
+    await library.send('wallet_switchEthereumChain', [switchArbitrumChain])
   } catch (switchChainError: any) {
     if (switchChainError.code === errChainNotAddedYet) {
       await addChain(library)
@@ -27,14 +49,8 @@ async function switchOrAddChain(library: JsonRpcProvider) {
 
 async function addChain(library: JsonRpcProvider) {
   try {
-    await library.send('wallet_addEthereumChain', [
-      {
-        chainId: arbitrumChainId,
-        chainName: 'Arbitrum',
-        rpcUrls: ['https://arb1.arbitrum.io/rpc'],
-      },
-    ])
+    await library.send('wallet_addEthereumChain', [addArbitrumChain])
   } catch (error: any) {
-    console.error(error)
+    return // error already logged by MetaMask
   }
 }
