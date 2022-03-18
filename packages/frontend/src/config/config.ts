d35dd347768f69e4f68aa6c2df85e153206d6352
Michał Sieczkowski <michal@ethworks.io>
2022-03-18 13:18:10 +0100

    Extract methods for getting dev and prod configs

--- packages/frontend/src/config/config.ts
+++ packages/frontend/src/config/config.ts
@@ -1,9 +1,31 @@
-import { ChainId } from '@usedapp/core'
+import { ChainId, Config as UseDAppConfig } from "@usedapp/core"
 
-export const CONFIG = {
-  useDAppConfig: {},
-  allowedNetworks:
-    import.meta.env.MODE === 'development'
-      ? [ChainId.Arbitrum, ChainId.ArbitrumRinkeby, ChainId.Localhost]
-      : [ChainId.Arbitrum],
+interface Config {
+  useDAppConfig: UseDAppConfig
+  allowedNetworks: number[]
+}
+
+export const CONFIG = getConfig(import.meta.env.MODE)
+
+function getConfig(environment: string): Config {
+  switch (environment) {
+    case 'development':
+      return getDevConfig()
+    default:
+      return getProdConfig()
+  }
+}
+
+function getDevConfig() {
+  return {
+    useDAppConfig: {},
+    allowedNetworks: [ChainId.Arbitrum, ChainId.ArbitrumRinkeby, ChainId.Localhost]
+  }
+}
+
+function getProdConfig() {
+  return {
+    useDAppConfig: {},
+    allowedNetworks: [ChainId.Arbitrum]
+  }
 }
