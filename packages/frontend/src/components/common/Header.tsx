490f884664f70c8a4577f279d87859eefd2cb6b0
Piotr Sadlik <sadlik512@gmail.com>
2022-03-18 09:51:26 +0100

    💀 Add bids page skeleton (#31)
    * Add route to the bids page
    
    * Add links back and forth
    
    * Extract header styles
    
    * Add basic container for page content
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

---
+++ packages/frontend/src/components/common/Header.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const HeaderBar = styled.header`
+  display: flex;
+  flex-shrink: 0;
+  width: 100%;
+  background: linear-gradient(to left, #7ec188 0%, #65c4e8 45.31%, #7779b5 100%);
+  position: relative;
+`
---
+++ packages/frontend/src/pages/Bids.tsx
@@ -0,0 +1,22 @@
+import { AllBidsList } from 'src/components/AllBids/AllBidsList'
+import { Header } from 'src/components/AllBids/Header'
+import { Colors } from 'src/styles/colors'
+import styled from 'styled-components'
+
+export const Bids = () => {
+  return (
+    <Body>
+      <Header />
+      <AllBidsList />
+    </Body>
+  )
+}
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: ${Colors.GreyLight};
+`
