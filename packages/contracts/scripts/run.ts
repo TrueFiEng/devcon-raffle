fb160884f0ba5367d10b2a634becfdae68732635
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-18 16:09:36 +0100

    Add bid task

--- packages/contracts/scripts/run.ts
+++ packages/contracts/scripts/run.ts
@@ -1,7 +1,7 @@
 import { deployDevcon, reservePrice } from "./deploy";
 import { Devcon6 } from "contracts";
-import { BigNumberish, constants, Signer, utils } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { bidAsSigner } from "scripts/utils/bid";
 
 const hre = require("hardhat")
 
@@ -25,20 +25,7 @@ async function run() {
 async function bid(devcon: Devcon6, signers: SignerWithAddress[]) {
   for (let i = 0; i < signers.length; i++) {
     await bidAsSigner(devcon, signers[i], reservePrice)
-    logBid(signers[i].address, reservePrice)
   }
-}
-
-async function bidAsSigner(devcon: Devcon6, signer: Signer, value: BigNumberish) {
-  await devcon.connect(signer).bid({ value })
-}
-
-function logBid(address: string, bidAmount: BigNumberish) {
-  console.log(`Account ${address} bid ${formatEther(bidAmount)}`)
-}
-
-function formatEther(balance: BigNumberish): string {
-  return `${utils.formatEther(balance).toString()}${constants.EtherSymbol}`
 }
 
 run()
--- packages/contracts/scripts/tasks.ts
+++ packages/contracts/scripts/tasks.ts
@@ -1,5 +1,10 @@
 import { task, types } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import { bidAsSigner } from "scripts/utils/bid";
+import { Devcon6__factory } from "contracts";
+import { devconAddress } from "scripts/utils/devcon";
+import { BigNumberish, constants, utils } from "ethers";
+import { parseEther } from "ethers/lib/utils";
 
 task("fast-forward", "Fast forwards a period of node's time")
   .addParam<number>("value", "Time in seconds to fast forward", undefined, types.int, false)
@@ -10,3 +15,26 @@ task("fast-forward", "Fast forwards a period of node's time")
     await provider.send('evm_increaseTime', [value])
     await provider.send('evm_mine')
   })
+
+task("bid", "Bids with given amount")
+  .addParam<string>("address", "The bidder's address")
+  .addParam<string>("amount", "The bid's amount",undefined, types.string)
+  .setAction(async (
+    { address, amount }: { address: string, amount: string },
+    hre
+  ) => {
+    const signer = await hre.ethers.getSigner(address)
+    const devcon = Devcon6__factory.connect(devconAddress, signer)
+
+    const ethAmount = parseEther(amount)
+    await bidAsSigner(devcon, signer, ethAmount)
+    logBid(address, ethAmount)
+  })
+
+function logBid(address: string, bidAmount: BigNumberish) {
+  console.log(`Account ${address} bid ${formatEther(bidAmount)}`)
+}
+
+function formatEther(amount: BigNumberish): string {
+  return `${utils.formatEther(amount).toString()}${constants.EtherSymbol}`
+}
