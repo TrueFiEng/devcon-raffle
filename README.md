796e37248e77ab432d42ca86fe65a0bb0c3d245c
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2022-03-22 15:01:20 +0100

    💾 Add scripts to settle auction and raffle (#41)
    * Add settle auction task
    
    * Add settle raffle task
    
    * Remove unnecessary types
    
    * Add settle tasks to package.json
    
    * Update README.md
    
    * Rename node commands in package.json

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
 ## Running Hardhat node
 To run the Hardhat node change directory to `contracts` and then execute:
 ```shell
-yarn local-node
+yarn node:run
 ```
 It runs Hardhat node, deploy contracts and place bid for the first twenty accounts.
 
 ## Interacting with contracts
 To interact with contracts change directory to `contracts`, then use below commands:
-- `yarn increase-time --value 100` - increases block time by *value* seconds
-- `yarn bid --address 0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc --amount 0.6` - bids as *address* with *amount* ETH
-- `yarn accounts` - prints list of available accounts
+- `yarn node:increase-time --value 100` - increases block time by *value* seconds
+- `yarn node:accounts` - prints list of available accounts
+- `yarn devcon:bid --address 0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc --amount 0.6` - bids as *address* with *amount* ETH
+- `yarn devcon:settle-auction --winners "[1,2]"` - settles auction and selects winners based on passed array of bidders IDs
+- `yarn devcon:settle-raffle` - settles raffle
 
 For additional information run below command:
 ```shell
--- packages/contracts/scripts/tasks.ts
+++ packages/contracts/scripts/tasks.ts
@@ -1,12 +1,15 @@
 import { task, types } from 'hardhat/config'
 import { devconAddress } from 'scripts/utils/devcon'
-import { BigNumberish, constants, utils } from 'ethers'
+import { BigNumber, BigNumberish, constants, Contract, utils } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { randomBigNumbers } from 'utils/bigNumber'
 
 const devconArtifactName = 'contracts/Devcon6.sol:Devcon6'
 
 task('increase-time', 'Increases block time')
-  .addParam<number>('value', 'Time in seconds to increase', undefined, types.int, false)
+  .addParam('value', 'Time in seconds to increase', undefined, types.int, false)
   .setAction(async ({ value }: { value: number }, hre) => {
     const provider = hre.network.provider
 
@@ -16,8 +19,8 @@ task('increase-time', 'Increases block time')
   })
 
 task('bid', 'Places bid for given account with provided amount')
-  .addParam<string>('address', 'The bidder\'s address')
-  .addParam<string>('amount', 'The bid\'s amount in ETH', undefined, types.string)
+  .addParam('address', 'The bidder\'s address')
+  .addParam('amount', 'The bid\'s amount in ETH', undefined, types.string)
   .setAction(async (
     { address, amount }: { address: string, amount: string },
     hre,
@@ -29,6 +32,29 @@ task('bid', 'Places bid for given account with provided amount')
     const ethAmount = parseEther(amount)
     await devcon.bid({ value: ethAmount })
     logBid(address, ethAmount)
+  })
+
+task('settle-auction', 'Settles auction')
+  .addParam('winners', 'Array of winners IDs', undefined, types.json)
+  .setAction(async (
+    { winners }: { winners: number[] },
+    hre,
+  ) => {
+    const devcon = await devconAsOwner(hre)
+
+    await devcon.settleAuction(winners)
+    console.log('Auction settled!')
+  })
+
+task('settle-raffle', 'Settles raffle')
+  .setAction(async (taskArgs, hre) => {
+    const devcon = await devconAsOwner(hre)
+
+    const raffleWinnersCount = await devcon.raffleWinnersCount()
+    const randomNumbersCount = BigNumber.from(raffleWinnersCount).div(8).toNumber()
+
+    await devcon.settleRaffle(randomBigNumbers(randomNumbersCount))
+    console.log('Raffle settled!')
   })
 
 task('accounts', 'Prints available accounts')
@@ -46,4 +72,15 @@ function logBid(address: string, bidAmount: BigNumberish) {
 
 function formatEther(amount: BigNumberish): string {
   return `${utils.formatEther(amount).toString()}${constants.EtherSymbol}`
+}
+
+async function devconAsOwner(hre: HardhatRuntimeEnvironment): Promise<Contract> {
+  const owner = await getDevconOwner(hre)
+  const devconFactory = await hre.ethers.getContractFactory(devconArtifactName)
+  return devconFactory.attach(devconAddress).connect(owner)
+}
+
+async function getDevconOwner(hre: HardhatRuntimeEnvironment): Promise<SignerWithAddress> {
+  const signers = await hre.ethers.getSigners()
+  return signers[0]
 }
