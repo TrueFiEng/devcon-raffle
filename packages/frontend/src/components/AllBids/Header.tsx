dd606dd312f522bad5bdaa63b92bbe1d3e79ffe9
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-03-22 11:42:20 +0100

    Change back button on bid page

--- packages/frontend/src/components/AllBids/Header.tsx
+++ packages/frontend/src/components/AllBids/Header.tsx
@@ -1,23 +1,20 @@
-import { useNavigate } from 'react-router-dom'
+
+import { BackButton } from 'src/components/Buttons/BackButton'
+import { HeaderBar } from 'src/components/common/Header'
+import { KeyIcon } from 'src/components/Icons/KeyIcon'
 import { Colors } from 'src/styles/colors'
 import styled from 'styled-components'
 
-import { Button } from '../Buttons/Button'
-import { HeaderBar } from '../common/Header'
-import { KeyIcon } from '../Icons/KeyIcon'
-
 export const Header = () => {
-  const navigate = useNavigate()
-
   return (
     <StyledHeader>
+      <BackButton url="/" />
       <Wrapper>
         <Title>
           <h2>Number of participants:</h2>
           <Number>234</Number>
         </Title>
       </Wrapper>
-      <BackButton onClick={() => navigate('/')}>Back</BackButton>
       <Key>
         <KeyIcon />
       </Key>
@@ -27,14 +24,13 @@ export const Header = () => {
 
 const StyledHeader = styled(HeaderBar)`
   height: 160px;
+  padding: 28px 68px;
   overflow: hidden;
 `
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: center;
   height: 100%;
   width: 100%;
   color: ${Colors.White};
@@ -44,6 +40,7 @@ const Title = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  row-gap: 8px;
 `
 
 const Key = styled.div`
@@ -55,14 +52,4 @@ const Key = styled.div`
 
 const Number = styled.h2`
   color: ${Colors.BlueDark};
-`
-
-const BackButton = styled(Button)`
-  position: absolute;
-  top: 36px;
-  left: 68px;
-  width: 84px;
-  color: ${Colors.BlueLight};
-  background-color: ${Colors.Transparent};
-  border: 1px solid ${Colors.BlueLight};
 `
