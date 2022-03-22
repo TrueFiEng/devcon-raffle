b97288801d1d532957dfcb7d5285ca71266e8df9
p-sad <sadlik512@gmail.com>
2022-03-22 16:05:59 +0100

    Replace interval with timeout loop

--- packages/frontend/src/providers/Bids/useBidEvents.ts
+++ packages/frontend/src/providers/Bids/useBidEvents.ts
@@ -4,28 +4,31 @@ import { useEffect, useMemo, useState } from 'react'
 
 const GANACHE_DEVCON6_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 const FIRST_QUERIED_BLOCK = 0
+const POLL_INTERVAL = 10_000
 
 export function useBidEvents() {
   const [events, setEvents] = useState<Log[]>([])
   const provider = useMemo(() => new JsonRpcProvider(), [])
   const [lastQueriedBlock, setLastQueriedBlock] = useState(FIRST_QUERIED_BLOCK)
 
+  const fetchEvents = async () => {
+    const fromBlock = lastQueriedBlock + 1
+    const toBlock = (await provider.getBlock('latest')).number
+    if (fromBlock < toBlock) {
+      const logs = await provider.getLogs({
+        address: GANACHE_DEVCON6_ADDRESS,
+        topics: [id('NewBid(address,uint256,uint256)')],
+        fromBlock,
+        toBlock,
+      })
+      setLastQueriedBlock(toBlock)
+      setEvents(events.concat(logs))
+    }
+    setTimeout(fetchEvents, POLL_INTERVAL)
+  }
+
   useEffect(() => {
-    const fetch = setInterval(async () => {
-      const fromBlock = lastQueriedBlock + 1
-      const toBlock = (await provider.getBlock('latest')).number
-      if (fromBlock < toBlock) {
-        const logs = await provider.getLogs({
-          address: GANACHE_DEVCON6_ADDRESS,
-          topics: [id('NewBid(address,uint256,uint256)')],
-          fromBlock,
-          toBlock,
-        })
-        setLastQueriedBlock(toBlock)
-        setEvents(events.concat(logs))
-      }
-    }, 10_000)
-    return () => clearInterval(fetch)
+    fetchEvents()
   }, [])
 
   return events
