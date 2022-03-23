eea5b2059bea120c490ee571b8ee75fb36c36b30
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-23 15:59:28 +0100

    Update chains in addresses constant

--- packages/frontend/src/constants/addresses.ts
+++ packages/frontend/src/constants/addresses.ts
@@ -2,11 +2,13 @@ import { ChainId } from '@usedapp/core'
 
 export const ADDRESSES = {
   multicall: {
-    [ChainId.Mainnet]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
-    [ChainId.Ropsten]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
+    [ChainId.Arbitrum]: '0x842eC2c7D803033Edf55E478F461FC547Bc54EB2',
+    [ChainId.ArbitrumRinkeby]: '0x5D6e06d3E154C5DBEC91317f0d04AE03AB49A273',
     [ChainId.Hardhat]: '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512',
   },
   devcon: {
+    [ChainId.Arbitrum]: undefined,
+    [ChainId.ArbitrumRinkeby]: undefined,
     [ChainId.Hardhat]: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
   },
 }
