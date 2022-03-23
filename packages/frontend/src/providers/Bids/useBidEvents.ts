6a22f9de0b713beb262ad025476a03a4716cc289
p-sad <sadlik512@gmail.com>
2022-03-23 13:48:11 +0100

    Use react-async-hook for async query

--- packages/frontend/src/providers/Bids/useBidEvents.ts
+++ packages/frontend/src/providers/Bids/useBidEvents.ts
@@ -2,6 +2,7 @@ import { id } from '@ethersproject/hash'
 import { JsonRpcProvider, Log } from '@ethersproject/providers'
 import { useEffect, useMemo, useState } from 'react'
 import 'react'
+import { useAsync } from 'react-async-hook'
 
 const GANACHE_DEVCON6_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 const FIRST_QUERIED_BLOCK = 0
@@ -11,43 +12,36 @@ export function useBidEvents() {
   const [events, setEvents] = useState<Log[]>([])
   const provider = useMemo(() => new JsonRpcProvider(), [])
   const [lastQueriedBlock, setLastQueriedBlock] = useState(FIRST_QUERIED_BLOCK)
-
-  const queryEvents = async () => {
-    await fetchEvents({ provider, lastQueriedBlock, setLastQueriedBlock, events, setEvents })
-    setTimeout(queryEvents, POLL_INTERVAL)
-  }
-
+  const { loading, result, execute, error } = useAsync(fetchEvents, [provider, FIRST_QUERIED_BLOCK])
   useEffect(() => {
-    queryEvents()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    if (loading) {
+      return
+    }
+    if (error) {
+      console.error(error)
+      setTimeout(() => execute(provider, lastQueriedBlock), POLL_INTERVAL)
+    }
+    if (result) {
+      setEvents((events) => events.concat(result.logs))
+      setLastQueriedBlock(result.lastQueriedBlock)
+      setTimeout(() => execute(provider, result.lastQueriedBlock), POLL_INTERVAL)
+    }
+  }, [loading, result, error, execute])
 
   return events
 }
 
-interface FetchEventsProps {
-  provider: JsonRpcProvider
-  lastQueriedBlock: number
-  setLastQueriedBlock: (n: number) => void
-  events: Log[]
-  setEvents: (events: Log[]) => void
-}
-
-async function fetchEvents({ provider, lastQueriedBlock, setLastQueriedBlock, events, setEvents }: FetchEventsProps) {
-  try {
-    const fromBlock = lastQueriedBlock + 1
-    const toBlock = (await provider.getBlock('latest')).number
-    if (fromBlock < toBlock) {
-      const logs = await provider.getLogs({
-        address: GANACHE_DEVCON6_ADDRESS,
-        topics: [id('NewBid(address,uint256,uint256)')],
-        fromBlock,
-        toBlock,
-      })
-      setLastQueriedBlock(toBlock)
-      setEvents(events.concat(logs))
-    }
-  } catch (e) {
-    console.error(e)
+async function fetchEvents(provider: JsonRpcProvider, lastQueriedBlock: number) {
+  let logs: Log[] = []
+  const fromBlock = lastQueriedBlock + 1
+  const toBlock = (await provider.getBlock('latest')).number
+  if (fromBlock <= toBlock) {
+    logs = await provider.getLogs({
+      address: GANACHE_DEVCON6_ADDRESS,
+      topics: [id('NewBid(address,uint256,uint256)')],
+      fromBlock,
+      toBlock,
+    })
   }
+  return { logs, lastQueriedBlock: toBlock }
 }
