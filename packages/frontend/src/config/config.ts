8ac94375014a2fb6904776990bf2da8305613361
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-23 09:41:46 +0100

    Fix lint

--- packages/frontend/src/config/config.ts
+++ packages/frontend/src/config/config.ts
@@ -1,9 +1,9 @@
 import { ChainId } from '@usedapp/core'
-import { ADDRESSES } from "src/constants/addresses"
+import { ADDRESSES } from 'src/constants/addresses'
 
 export const CONFIG = {
   useDAppConfig: {
-    multicallAddresses: ADDRESSES.multicall
+    multicallAddresses: ADDRESSES.multicall,
   },
   allowedNetworks:
     import.meta.env.MODE === 'development'
--- packages/frontend/src/constants/addresses.ts
+++ packages/frontend/src/constants/addresses.ts
@@ -1,9 +1,9 @@
-import { ChainId } from "@usedapp/core"
+import { ChainId } from '@usedapp/core'
 
 export const ADDRESSES = {
   multicall: {
     [ChainId.Mainnet]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
     [ChainId.Ropsten]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
     [ChainId.Hardhat]: '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512',
-  }
+  },
 }
