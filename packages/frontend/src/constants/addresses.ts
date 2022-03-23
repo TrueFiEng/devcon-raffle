fffffdd2b4943a51377e70b49dc8504a3662fbb3
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-23 09:45:23 +0100

    Add devcon hardhat address

--- packages/frontend/src/constants/addresses.ts
+++ packages/frontend/src/constants/addresses.ts
@@ -6,4 +6,7 @@ export const ADDRESSES = {
     [ChainId.Ropsten]: '0x5ba1e12693dc8f9c48aad8770482f4739beed696',
     [ChainId.Hardhat]: '0xe7f1725e7734ce288f8367e1bb143e90bb3f0512',
   },
+  devcon: {
+    [ChainId.Hardhat]: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  },
 }
