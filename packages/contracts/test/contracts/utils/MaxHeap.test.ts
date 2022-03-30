7b3ae57b9e6f3d4a9c1466ac01093662bf8fb2bc
Michał Sieczkowski <michal@ethworks.io>
2022-03-30 15:04:35 +0200

    Extract insert function in tests

--- packages/contracts/test/contracts/utils/MaxHeap.test.ts
+++ packages/contracts/test/contracts/utils/MaxHeap.test.ts
@@ -13,6 +13,12 @@ describe('MaxHeap', function() {
     ({ heap } = await loadFixture(maxHeapMockFixture))
   })
 
+  async function insert(keys: number[]) {
+    for (const key of keys) {
+      await heap.insert(key)
+    }
+  }
+
   describe('insert', function() {
     it('adds the first element properly', async function() {
       await heap.insert(1)
@@ -20,31 +26,22 @@ describe('MaxHeap', function() {
     })
 
     it('can swap left child with parent', async function() {
-      await heap.insert(1)
-      await heap.insert(2)
+      await insert([1, 2])
       expect(await heap.getArray()).to.deep.eq(bigNumberArrayFrom([2, 1]))
     })
 
     it('can swap right child with parent', async function() {
-      await heap.insert(2)
-      await heap.insert(1)
-      await heap.insert(3)
+      await insert([2, 1, 3])
       expect(await heap.getArray()).to.deep.eq(bigNumberArrayFrom([3, 1, 2]))
     })
 
     it('can bubble more then one level up', async function() {
-      await heap.insert(2)
-      await heap.insert(1)
-      await heap.insert(3)
-      await heap.insert(4)
+      await insert([2, 1, 3, 4])
       expect(await heap.getArray()).to.deep.eq(bigNumberArrayFrom([4, 3, 2, 1]))
     })
 
     it('knows when to stop bubbling up', async function() {
-      await heap.insert(2)
-      await heap.insert(4)
-      await heap.insert(1)
-      await heap.insert(3)
+      await insert([2, 4, 1, 3])
       expect(await heap.getArray()).to.deep.eq(bigNumberArrayFrom([4, 3, 1, 2]))
     })
   })
