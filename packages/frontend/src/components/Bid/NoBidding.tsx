f837a996b9fd996fc73d19e4b94359c6c992c5f0
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-03-30 16:08:15 +0200

    Change no bidding text

--- packages/frontend/src/components/Bid/NoBidding.tsx
+++ packages/frontend/src/components/Bid/NoBidding.tsx
@@ -1,15 +1,16 @@
-import { FormWrapper } from 'src/components/Form/Form'
+import { FormWrapper, FormHeading, FormText } from 'src/components/Form/Form'
 import styled from 'styled-components'
 
 export const NoBidding = () => {
   return (
-    <BidStartWrapper>
-      <h2>Wait for results ⏳</h2>
-    </BidStartWrapper>
+    <Wrapper>
+      <FormHeading>Bidding time is over ⌛️</FormHeading>
+      <FormText>Try your luck next year!</FormText>
+    </Wrapper>
   )
 }
 
-const BidStartWrapper = styled(FormWrapper)`
-  padding: 0 0 0 170px;
+const Wrapper = styled(FormWrapper)`
+  padding: 0 0 0 115px;
   justify-content: center;
 `
