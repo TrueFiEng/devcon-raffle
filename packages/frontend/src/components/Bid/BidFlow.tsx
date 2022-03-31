e8a6ba389bb23bf44a59798be2d88baaf4b4e9d0
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-03-31 10:30:22 +0200

    Make useAuctionTime work using any

--- packages/frontend/src/components/Bid/BidFlow.tsx
+++ packages/frontend/src/components/Bid/BidFlow.tsx
@@ -4,7 +4,12 @@ import { PlaceBidFlow } from 'src/components/Bid/PlaceBid/PlaceBidFlow'
 import { WinBidFlow } from 'src/components/Bid/WinBid/WinBidFlow'
 import { useUserBid } from 'src/hooks/useUserBid'
 
+import { useAuctionTime } from '../../hooks/useAuctionTime'
+
 export const BidFlow = () => {
+  const {value, error } = useAuctionTime()
+  console.log(value)
+  console.log(error)
   const userBid = useUserBid()
   const timeIsOver = false
 
--- packages/frontend/src/hooks/useAuctionTime.ts
+++ packages/frontend/src/hooks/useAuctionTime.ts
@@ -1,13 +1,15 @@
+import { BigNumber } from '@ethersproject/bignumber'
 import { useCall } from '@usedapp/core'
 
 import { useDevconContract } from './contract'
 
-function useAuctionTime() {
+export function useAuctionTime() {
   const devconContract = useDevconContract()
-  const { value, error } = useCall({
-    contract: devconContract as any,
-    method: 'biddingStartTime',
-    args: [],
-  })
-  return { value, error }
+  const { value, error } =
+    useCall({
+      contract: devconContract as any,
+      method: 'biddingStartTime',
+      args: [],
+    }) ?? {}
+  return { value: value as BigNumber, error }
 }
