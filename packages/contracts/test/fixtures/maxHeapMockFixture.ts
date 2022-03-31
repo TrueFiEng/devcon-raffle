8df355d084703a2cf384f1e75bddb31eb23b8f7a
Michał Sieczkowski <michal@ethworks.io>
2022-03-31 15:09:10 +0200

    Extract and use deployMaxHeapLibrary

--- packages/contracts/test/fixtures/maxHeapMockFixture.ts
+++ packages/contracts/test/fixtures/maxHeapMockFixture.ts
@@ -4,14 +4,18 @@ import { MaxHeapMock__factory } from 'contracts'
 import { ethers } from 'hardhat'
 
 export async function maxHeapMockFixture([deployer]: Wallet[], provider: MockProvider) {
-  const heapLibraryFactory = await ethers.getContractFactory('MaxHeap')
-  const heapLibrary = await heapLibraryFactory.deploy()
-
-  const libraryLink = {
-    'contracts/utils/MaxHeap.sol:MaxHeap': heapLibrary.address,
-    __$f92e1b546cb81b0df9056e27145904c2f5$__: heapLibrary.address,
-  }
+  const libraryLink = await deployMaxHeapLibrary()
   const heap = await new MaxHeapMock__factory(libraryLink, deployer).deploy()
 
   return { heap, provider }
+}
+
+export async function deployMaxHeapLibrary() {
+  const heapLibraryFactory = await ethers.getContractFactory('MaxHeap')
+  const heapLibrary = await heapLibraryFactory.deploy()
+
+  return {
+    'contracts/utils/MaxHeap.sol:MaxHeap': heapLibrary.address,
+    __$f92e1b546cb81b0df9056e27145904c2f5$__: heapLibrary.address,
+  }
 }
