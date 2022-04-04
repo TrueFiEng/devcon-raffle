8ba1dd04f4cb79077079e8870ab05cf190705bcf
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-04-04 14:41:07 +0200

    Return `padZeroes` formatting

--- packages/frontend/src/utils/formatters/formatTimeLeft.ts
+++ packages/frontend/src/utils/formatters/formatTimeLeft.ts
@@ -1,14 +1,16 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import moment from 'moment'
 
+import { padZeroes } from './padZeroes'
+
 export function formatTimeLeft(timestamp: BigNumber, now = Date.now()) {
   const date = moment(new Date(timestamp.mul(1000).toNumber()).toISOString())
-  const difference = date.diff(now)
+  const difference = date.diff(now) > 0 ? date.diff(now) : 0
   const duration = moment.duration(difference)
 
-  const days = duration.days()
-  const hours = duration.hours()
-  const minutes = duration.minutes()
+  const days = padZeroes(duration.days())
+  const hours = padZeroes(duration.hours())
+  const minutes = padZeroes(duration.minutes())
 
   return `${days}d ${hours}h ${minutes}m`
 }
