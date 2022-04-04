2ade72f65f7bef20c2a16509d3764e3dfb594da9
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-04 11:54:20 +0200

    Use milliseconds in formatEndDate

--- packages/frontend/src/utils/formatters/formatEndDate.ts
+++ packages/frontend/src/utils/formatters/formatEndDate.ts
@@ -2,6 +2,6 @@ import { BigNumber } from '@ethersproject/bignumber'
 import moment from 'moment'
 
 export function formatEndDate(timestamp: BigNumber) {
-  const date = new Date(timestamp.toNumber()).toISOString()
+  const date = new Date(timestamp.mul(1000).toNumber()).toISOString()
   return moment(date).format('DD.MM')
 }
