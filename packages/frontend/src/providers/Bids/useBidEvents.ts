11eb21f329366b83e9c58266dcc407942216dbf5
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-04 16:54:18 +0200

    Update devcon address

--- packages/frontend/src/providers/Bids/useBidEvents.ts
+++ packages/frontend/src/providers/Bids/useBidEvents.ts
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from 'react'
 import 'react'
 import { useAsync } from 'react-async-hook'
 
-const GANACHE_DEVCON6_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const GANACHE_DEVCON6_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 const FIRST_QUERIED_BLOCK = 0
 const POLL_INTERVAL = 10_000
 
