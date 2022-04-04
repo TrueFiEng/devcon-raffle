536f73b44bfe361e979c3549abc1222b109d746f
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-04-04 14:24:28 +0200

    Fix date in awaiting state

--- packages/frontend/src/utils/formatters/formatEndDateText.ts
+++ packages/frontend/src/utils/formatters/formatEndDateText.ts
@@ -2,7 +2,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 import moment from 'moment'
 
 export function formatEndDateText(timestamp: BigNumber) {
-  const date = new Date(timestamp.toNumber()).toISOString()
+  const date = new Date(timestamp.mul(1000).toNumber()).toISOString()
   const dateText = moment(date).format('MMMM Do')
   const timeText = moment(date).format('hh A')
   return { dateText, timeText }
