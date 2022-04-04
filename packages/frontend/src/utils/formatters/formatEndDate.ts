4a7e8fcd5874a9bcc0cd966f3b6a95c6e4759cb3
Maria Rushkova <66270386+mrushkova@users.noreply.github.com>
2022-04-04 15:36:11 +0200

    ⏰  Change time formatting (#76)
    * Use current time without contract
    
    * Fix date in awaiting state
    
    * Use moment.js for `formatTimeLeft`
    
    * Return `padZeroes` formatting
    
    * Remove test check for excess seconds
    
    * Extract second const in script
    
    * Simplify date conversion
    
    Co-authored-by: Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>

--- packages/frontend/src/utils/formatters/formatEndDate.ts
+++ packages/frontend/src/utils/formatters/formatEndDate.ts
@@ -2,6 +2,5 @@ import { BigNumber } from '@ethersproject/bignumber'
 import moment from 'moment'
 
 export function formatEndDate(timestamp: BigNumber) {
-  const date = new Date(timestamp.mul(1000).toNumber()).toISOString()
-  return moment(date).format('DD.MM')
+  return moment.unix(timestamp.toNumber()).format('DD.MM')
 }
