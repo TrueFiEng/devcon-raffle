a37bd5ed4ba9295b2e6a7e09d56bc4feb71d2292
Michał Sieczkowski <michal@ethworks.io>
2022-04-06 14:24:27 +0200

    Rename variable

--- packages/frontend/src/hooks/useAuctionTime.ts
+++ packages/frontend/src/hooks/useAuctionTime.ts
@@ -6,8 +6,8 @@ import { useDevconParam } from './useDevconParam'
 
 export function useAuctionTime() {
   const { state } = useContractState()
-  const getTime = state === ContractState.AWAITING_BIDDING ? 'biddingStartTime' : 'biddingEndTime'
-  const { devconValue } = useDevconParam(getTime)
+  const method = state === ContractState.AWAITING_BIDDING ? 'biddingStartTime' : 'biddingEndTime'
+  const { devconValue } = useDevconParam(method)
 
   const timestamp = devconValue ? devconValue : BigNumber.from(moment().unix())
   return { timestamp }
