8d4621cd73d0d247faf0ca02009530d6fe2bcb4f
Michał Sieczkowski <michal@ethworks.io>
2022-04-06 17:00:56 +0200

    Fix lint

--- packages/frontend/src/hooks/useClaimingEndTime.ts
+++ packages/frontend/src/hooks/useClaimingEndTime.ts
@@ -7,9 +7,7 @@ export function useClaimingEndTime() {
   const { devconValue, error } = useDevconParam('claimingEndTime')
 
   const claimingEndTime = useMemo(() => {
-    return devconValue
-      ? moment.unix(devconValue.toNumber()).format('DD.MM.YYYY')
-      : '--.--.----'
+    return devconValue ? moment.unix(devconValue.toNumber()).format('DD.MM.YYYY') : '--.--.----'
   }, [devconValue])
 
   return { claimingEndTime, error }
