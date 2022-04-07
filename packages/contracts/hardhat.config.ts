eece6f28ec315fead0ba38a9d13f8e2773f10250
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-07 08:57:26 +0200

    Add arbitrum rinkeby network to hardhat config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -4,39 +4,47 @@ import './abi-exporter'
 import 'tsconfig-paths/register'
 import 'hardhat-gas-reporter'
 import 'scripts/tasks'
+import 'scripts/testnetTasks'
 
 import mocharc from './.mocharc.json'
 import compiler from './.compiler.json'
 
+require('dotenv').config({ path: '../../.env' })
+
 module.exports = {
-    paths: {
-        sources: './contracts',
-        artifacts: './build',
-        cache: './cache',
+  paths: {
+    sources: './contracts',
+    artifacts: './build',
+    cache: './cache'
+  },
+  abiExporter: {
+    path: './build',
+    flat: true,
+    spacing: 2
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      initialDate: '2022-01-01T00:00:00',
+      allowUnlimitedContractSize: true,
+      accounts: {
+        count: 40
+      }
     },
-    abiExporter: {
-        path: './build',
-        flat: true,
-        spacing: 2,
-    },
-    networks: {
-        hardhat: {
-            initialDate: '2022-01-01T00:00:00',
-            allowUnlimitedContractSize: true,
-            accounts: {
-              count: 40,
-            }
-        },
-    },
-    typechain: {
-        outDir: 'build/types',
-        target: 'ethers-v5',
-    },
-    solidity: {
-        compilers: [compiler],
-    },
-    mocha: {
-        ...mocharc,
-        timeout: 400000,
-    },
+    rinkeby: {
+      url: 'https://rinkeby.arbitrum.io/rpc',
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  typechain: {
+    outDir: 'build/types',
+    target: 'ethers-v5'
+  },
+  solidity: {
+    compilers: [compiler]
+  },
+  mocha: {
+    ...mocharc,
+    timeout: 400000
+  }
 }
