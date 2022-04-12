a75e26c7d3e2e1d64cbbdb09ef5632567bbf2f5e
Michał Sieczkowski <michal@ethworks.io>
2022-04-12 16:06:40 +0200

    Add a script for generating dotenv file

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ A number of custom Hardhat tasks were defined to aid testing.
 - `yarn hardhat:settle-raffle` - settle raffle using random numbers
 
 #### Arbitrum Rinkeby
+- `yarn rinkeby:generate-dotenv [--path <STRING>] [--count <INT>]` - generate .env file needed for other tasks, *path* - output path, *count* - number of private keys to generate
 - `yarn rinkeby:transfer-ether` - transfer ether from `DEPLOYER` to `PRIVATE_KEYS` accounts
 - `yarn rinkeby:deploy [--delay <INT>]` - deploy Devcon6 contract, *delay* in seconds can be set to push back bidding start time
 - `yarn rinkeby:init-bids` - place initial bids using `PRIVATE_KEYS` accounts
--- packages/contracts/scripts/tasks/devcon/rinkebyTasks.ts
+++ packages/contracts/scripts/tasks/devcon/rinkebyTasks.ts
@@ -3,9 +3,21 @@ import { deployTestnetDevcon } from 'scripts/deploy/deploy'
 import { BigNumberish, constants, Contract, Signer, utils, Wallet } from 'ethers'
 import { formatEther, parseEther } from 'ethers/lib/utils'
 import { connectToDevcon } from 'scripts/utils/devcon'
+import writeFileAtomic from 'write-file-atomic'
 
 const testnetDevconAddress = '0x0cD6783fca8D1a9DbAa6404B759b0C2110f3C2A0'
 const testnetHeapAddress = '0xc1D8b72838Cb3F1c52651d76ea186Df457817aD6'
+
+task('generate-dotenv', 'Generate .env file needed for other tasks')
+  .addParam('path', 'location of the file', '../../.env', types.string)
+  .addParam('count', 'number of wallets to generate', 20, types.int)
+  .setAction(async ({ path, count }: { path: string, count: number }, hre) => {
+    const keys = Array(count).fill('').map(_ => Wallet.createRandom().privateKey)
+    const deployer = hre.config.networks.rinkeby.accounts[0]
+    const output = createDotenv(deployer, keys)
+    console.log(output)
+    await writeFileAtomic(path, output)
+  })
 
 task('transfer-ether', 'Transfers ether from DEPLOYER account to PRIVATE_KEYS accounts')
   .addParam('value', 'ETH amount to send', '0.001', types.string, true)
@@ -58,4 +70,12 @@ export async function bidAs(devcon: Contract, signer: Signer, value: BigNumberis
   const tx = await devconAsSigner.bid({ value })
   const { gasUsed } = await tx.wait()
   console.log(`Bid ${value.toString()} wei as ${await signer.getAddress()}, gasUsed: ${gasUsed.toString()}`)
+}
+
+function createDotenv(deployer: string, keys: string[]): string {
+  return `MODE=development
+DEPLOYER=${deployer}
+PRIVATE_KEYS='[
+${keys.map(key => `  "${key}",`).join('\n')}
+]'`
 }
