63a72614adc92760fbf6aac657ebb2dfb8a5f95c
Michał Sieczkowski <michal@ethworks.io>
2022-04-14 13:32:09 +0200

    Fix useProvider

--- packages/frontend/src/hooks/contract/useProvider.ts
+++ packages/frontend/src/hooks/contract/useProvider.ts
@@ -1,13 +1,14 @@
 import { Provider } from '@ethersproject/providers'
 import { useEthers } from '@usedapp/core'
-import { useReadonlyNetwork } from '@usedapp/core/dist/cjs/src/hooks'
+import { useReadonlyNetworks } from '@usedapp/core/internal'
 import { useReadOnlyChainId } from 'src/hooks/chainId/useReadOnlyChainId'
 
 export function useProvider(): Provider {
   const { library: injectedProvider, error } = useEthers()
 
+  const providers = useReadonlyNetworks()
   const chainId = useReadOnlyChainId()
-  const { provider: readOnlyProvider } = useReadonlyNetwork({chainId}) ?? {}
+  const readOnlyProvider = providers[chainId]
 
   if (readOnlyProvider === undefined) {
     throw new Error('readOnlyProvider is not defined')
