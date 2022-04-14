baf4a8f76e32a8ac33bef74198933ff6f0835ee1
Daniel Izdebski <spidiritsu@gmail.com>
2022-04-14 15:06:50 +0200

    Replace useEffect with useMemo in useContractBids hook

--- packages/frontend/src/hooks/useContractBids.ts
+++ packages/frontend/src/hooks/useContractBids.ts
@@ -1,21 +1,15 @@
 import { useCall } from '@usedapp/core'
-import { useEffect, useState } from 'react'
+import { useMemo } from 'react'
 import { ZERO } from 'src/constants/bigNumber'
 import { Bid } from 'src/models/Bid'
 
 import { useDevconContract } from './contract'
 
 export function useContractBids() {
-  const [bids, setBids] = useState<Bid[]>([])
-
   const fetchedBids = useBidsWithAddresses()
   const totalAmountAsString = fetchedBids.reduce((totalAmount, bid) => totalAmount.add(bid.amount), ZERO).toString()
 
-  useEffect(() => {
-    setBids(fetchedBids)
-  }, [totalAmountAsString]) // eslint-disable-line react-hooks/exhaustive-deps
-
-  return bids
+  return useMemo(() => fetchedBids, [totalAmountAsString])
 }
 
 function useBidsWithAddresses(): Bid[] {
