26907ee0bbd383bce212e5aa5a910723acbc31ab
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-19 13:07:18 +0200

    Extract getChain function

--- packages/frontend/src/utils/getExplorerLink.ts
+++ packages/frontend/src/utils/getExplorerLink.ts
@@ -2,11 +2,15 @@ import { Arbitrum, ChainId } from '@usedapp/core'
 import { getChainById } from '@usedapp/core/internal'
 
 export function getExplorerAddressLink(chainId: ChainId, address: string) {
-  const chain = getChainById(chainId) ?? Arbitrum
+  const chain = getChain(chainId)
   return chain.getExplorerAddressLink(address)
 }
 
 export function getExplorerTxLink(chainId: ChainId, txHash: string) {
-  const chain = getChainById(chainId) ?? Arbitrum
+  const chain = getChain(chainId)
   return chain.getExplorerTransactionLink(txHash)
+}
+
+function getChain(chainId: ChainId) {
+  return getChainById(chainId) ?? Arbitrum
 }
