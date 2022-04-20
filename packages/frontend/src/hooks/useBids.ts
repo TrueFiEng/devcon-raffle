d37c228683d0b33e3a6ad282a86ef9f96abdacd8
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-20 16:19:07 +0200

    Return correct data in useBids hook

--- packages/frontend/src/hooks/useBids.ts
+++ packages/frontend/src/hooks/useBids.ts
@@ -1,4 +1,7 @@
-import { useContext } from 'react'
+import { useContext, useMemo } from 'react'
 import { BidsContext } from 'src/providers/Bids'
 
-export const useBids = () => useContext(BidsContext)
+export const useBids = () => {
+  const context = useContext(BidsContext)
+  return useMemo(() => context.bidsState, [context])
+}
