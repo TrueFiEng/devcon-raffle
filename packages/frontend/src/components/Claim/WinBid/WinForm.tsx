a4f7eacb12bb3e2e1a818abd13e7e9403f38c586
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2022-04-20 12:52:27 +0200

    🎯 Center voucher code for auction winner (#111)
    * Fix typo in auction winner text
    
    * Simplify WinForm

--- packages/frontend/src/components/Claim/WinBid/WinForm.tsx
+++ packages/frontend/src/components/Claim/WinBid/WinForm.tsx
@@ -2,6 +2,7 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { TxFlowSteps } from 'src/components/Auction/TxFlowSteps'
 import { VoucherForm } from 'src/components/Claim/WinBid/VoucherForm'
 import { WinBidForm } from 'src/components/Claim/WinBid/WinBidForm'
+import { WinType } from 'src/components/Claim/WinBid/WinFlowEnum'
 import { FormWrapper } from 'src/components/Form/Form'
 import { UserBid } from 'src/models/Bid'
 import { Colors } from 'src/styles/colors'
@@ -16,39 +17,41 @@ interface WinFormProps {
 }
 
 export const WinForm = ({ userBid, withdrawalAmount, setView, voucher, setVoucher }: WinFormProps) => {
+  if (!voucher) {
+    return (
+      <Wrapper>
+        <WinBidForm
+          userBid={userBid}
+          withdrawalAmount={withdrawalAmount}
+          setView={setView}
+          voucher={voucher}
+          setVoucher={setVoucher}
+        />
+      </Wrapper>
+    )
+  }
+
   return (
     <>
-      {voucher ? (
-        userBid.claimed ? (
-          <Wrapper>
-            <VoucherForm voucher="0xD69bcE4E8D0929E16" withdrawnBid={userBid.claimed} />{' '}
-          </Wrapper>
-        ) : (
-          <WrapperRow>
-            <WinFormWrapper>
-              <WinBidForm
-                userBid={userBid}
-                withdrawalAmount={withdrawalAmount}
-                setView={setView}
-                voucher={voucher}
-                setVoucher={setVoucher}
-              />
-            </WinFormWrapper>
-            <VoucherFormWrapper>
-              <VoucherForm voucher="0xD69bcE4E8D0929E16" withdrawnBid={userBid.claimed} />
-            </VoucherFormWrapper>
-          </WrapperRow>
-        )
-      ) : (
+      {userBid.claimed || userBid.winType === WinType.Auction ? (
         <Wrapper>
-          <WinBidForm
-            userBid={userBid}
-            withdrawalAmount={withdrawalAmount}
-            setView={setView}
-            voucher={voucher}
-            setVoucher={setVoucher}
-          />
+          <VoucherForm voucher="0xD69bcE4E8D0929E16" withdrawnBid={userBid.claimed} />{' '}
         </Wrapper>
+      ) : (
+        <WrapperRow>
+          <WinFormWrapper>
+            <WinBidForm
+              userBid={userBid}
+              withdrawalAmount={withdrawalAmount}
+              setView={setView}
+              voucher={voucher}
+              setVoucher={setVoucher}
+            />
+          </WinFormWrapper>
+          <VoucherFormWrapper>
+            <VoucherForm voucher="0xD69bcE4E8D0929E16" withdrawnBid={userBid.claimed} />
+          </VoucherFormWrapper>
+        </WrapperRow>
       )}
     </>
   )
