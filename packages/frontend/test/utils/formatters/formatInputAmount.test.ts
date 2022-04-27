cd386b2e516d196b0475045a0abaf969400b3fbe
Daniel Izdebski <spidiritsu@gmail.com>
2022-04-27 15:59:19 +0200

    Fix typo

--- packages/frontend/test/utils/formatters/formatInputAmount.test.ts
+++ packages/frontend/test/utils/formatters/formatInputAmount.test.ts
@@ -1,15 +1,15 @@
 import { formatInputAmount } from 'src/utils/formatters/formatInputAmount'
 
 describe('formatInputAmount', () => {
-  it('Handles integers start with 0', () => {
+  it('Handles integers starting with 0', () => {
     expect(formatInputAmount('09887777')).toBe('9887777')
   })
 
-  it('Handles integers start with .', () => {
+  it('Handles integers starting with .', () => {
     expect(formatInputAmount('.9887777')).toBe('0.9887777')
   })
 
-  it('Handles integers end with .', () => {
+  it('Handles integers ending with .', () => {
     expect(formatInputAmount('9887777.')).toBe('9887777')
   })
 
