e1e127633aa856660fe15c1ead4925bf70c36a50
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-04-28 15:35:09 +0200

    Add explanatory TODO in BidFlow.tsx

--- packages/frontend/src/components/Bid/BidFlow.tsx
+++ packages/frontend/src/components/Bid/BidFlow.tsx
@@ -11,5 +11,7 @@ export const BidFlow = () => {
   const endInitialBidding = useCallback(() => setIsInitialBid(false), [setIsInitialBid])
   useEffect(() => setIsInitialBid(!userBid), [account]) // eslint-disable-line react-hooks/exhaustive-deps
 
+  // TODO: useUserBids returns actual user bid after this component is rendered
+  // it causes displaying PlaceBidFlow for users that have already placed bid
   return isInitialBid ? <PlaceBidFlow endInitialBidding={endInitialBidding} /> : <BumpBidFlow />
 }
