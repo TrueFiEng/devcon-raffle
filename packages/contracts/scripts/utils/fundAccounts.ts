f74baac1a0b8708620a184e3ccd80ea83c4e5e29
Michał Sieczkowski <michal@ethworks.io>
2022-04-29 14:41:24 +0200

    Fix fundAccounts function

--- packages/contracts/scripts/utils/fundAccounts.ts
+++ packages/contracts/scripts/utils/fundAccounts.ts
@@ -3,14 +3,16 @@ import { BigNumber, providers, Wallet } from 'ethers'
 import { parseEther } from 'ethers/lib/utils'
 
 export async function fundAccounts(genesisSigner: SignerWithAddress, accounts: Wallet[], reservePrice: BigNumber, minBidIncrement: BigNumber) {
-  const txs: Promise<providers.TransactionResponse>[] = []
+  const txs: Promise<providers.TransactionReceipt>[] = []
 
   for (let i = 0; i < accounts.length; i++) {
     const ethRequired = reservePrice.add(minBidIncrement.mul(i)).add(parseEther('0.1')) // Adding 0.1 ETH for gas
-    txs.push(genesisSigner.sendTransaction({
-      to: accounts[i].address,
-      value: ethRequired,
-    }))
+    txs.push(
+      genesisSigner.sendTransaction({
+        to: accounts[i].address,
+        value: ethRequired,
+      }).then(tx => tx.wait()),
+    )
   }
 
   await Promise.all(txs)
