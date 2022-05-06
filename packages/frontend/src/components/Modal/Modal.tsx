5a61eef81af019d37b19d0b7756373cdf0fbd811
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-05-06 17:33:16 +0200

    Add modal component

---
+++ packages/frontend/src/components/Modal/Modal.tsx
@@ -0,0 +1,67 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { ReactNode, useCallback } from 'react'
+import { hexOpacity, Colors } from 'src/styles/colors'
+import styled from 'styled-components'
+
+import { CloseButton } from '../Buttons/CloseButton'
+import { HeaderBar } from '../common/Header'
+import { Logo } from '../Icons/Logo'
+
+export interface ModalProps {
+  isShown: boolean | undefined
+  onRequestClose: () => void
+  children: ReactNode
+}
+
+export const Modal = ({ isShown, onRequestClose, children }: ModalProps) => {
+  const closeModal = useCallback(() => isShown && onRequestClose(), [isShown, onRequestClose])
+
+  return (
+    <Dialog.Root open={isShown}>
+      <Dialog.Portal>
+        <Overlay>
+          <Content>
+            <Header>
+              <Logo />
+              <Close>
+                <CloseButton size={24} color={Colors.White} onClick={closeModal} />
+              </Close>
+            </Header>
+            {children}
+          </Content>
+        </Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
+
+const Overlay = styled(Dialog.Overlay)`
+  background-color: ${hexOpacity(Colors.Blue, 0.8)};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: grid;
+  place-items: center;
+  overflow-y: hidden;
+`
+
+const Content = styled(Dialog.Content)`
+  width: 383px;
+  background-color: ${Colors.White};
+`
+const Header = styled(HeaderBar)`
+  justify-content: center;
+  align-items: center;
+  height: 75px;
+  overflow: hidden;
+`
+
+const Close = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 8px;
+  margin-left: auto;
+`
