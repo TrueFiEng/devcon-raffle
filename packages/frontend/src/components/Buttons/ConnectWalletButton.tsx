60eb1bdb8635e1f83fe8fa270fe57abc18753498
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-05-11 15:13:18 +0200

    Remove wallet modal from `ConnectWalletButton`

--- packages/frontend/src/components/Buttons/ConnectWalletButton.tsx
+++ packages/frontend/src/components/Buttons/ConnectWalletButton.tsx
@@ -1,7 +1,4 @@
 import { useEthers } from '@usedapp/core'
-import { useModal } from 'src/hooks/useModal'
-
-import { AddWalletModal } from '../Modal/AddWalletModal'
 
 import { Button, ButtonProps } from './Button'
 
@@ -9,20 +6,12 @@ type ConnectWalletButtonProps = Omit<ButtonProps, 'onClick' | 'children'>
 
 export const ConnectWalletButton = (props: ConnectWalletButtonProps) => {
   const { activateBrowserWallet } = useEthers()
-  const { isShown, toggle } = useModal()
-
-  function detectMetaMask() {
-    const { web3, ethereum } = window as any
-    if (!ethereum && !web3) return false
-    return ethereum?.isMetaMask || web3?.currentProvider?.isMetaMask
-  }
 
   return (
     <>
-      <Button {...props} onClick={detectMetaMask() ? activateBrowserWallet : toggle}>
+      <Button {...props} onClick={activateBrowserWallet}>
         Connect Wallet
       </Button>
-      {<AddWalletModal isShown={isShown} onRequestClose={toggle} />}
     </>
   )
 }
