ee553138999d76ef1c4b4b8a74a31b63d3bd0e9f
Daniel Izdebski <spidiritsu@gmail.com>
2022-05-12 16:25:23 +0200

    Add Web3Modal provider

---
+++ packages/frontend/src/providers/Web3Modal/provider.tsx
@@ -0,0 +1,73 @@
+import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk'
+import Portis from '@portis/web3'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { ReactNode } from 'react'
+import { CONFIG } from 'src/config/config'
+import { WALLET_CONNECT_BRIDGE_URL } from 'src/constants/walletConnectBridgeUrl'
+import { useDefaultNetwork } from 'src/hooks/chain/useDefaultNetwork'
+import { useWhichWallet } from 'src/hooks/useWhichWallet'
+import Web3Modal from 'web3modal'
+
+import { Web3ModalContext } from './context'
+
+interface Props {
+  children: ReactNode
+}
+
+export const Web3ModalProvider = ({ children }: Props) => {
+  const { isBraveWallet } = useWhichWallet()
+  const { chainName, chainId, rpcUrl } = useDefaultNetwork()
+
+  const injectedOptions = {
+    display: {
+      name: isBraveWallet ? 'BraveWallet' : 'MetaMask',
+      description: 'Connect with the provider in your Browser',
+    },
+    package: null,
+  }
+
+  const walletConnectOptions = {
+    bridge: WALLET_CONNECT_BRIDGE_URL,
+    rpc: {
+      [chainId]: rpcUrl,
+    },
+  }
+
+  const portisOptions = {
+    network: {
+      nodeUrl: rpcUrl,
+      chainId: chainId,
+    },
+    id: CONFIG.portisDAppID,
+  }
+
+  const coinbaseWalletOptions = {
+    appName: CONFIG.dappName,
+    rpc: rpcUrl,
+    chainId: chainId,
+    darkMode: false,
+  }
+
+  const providerOptions = {
+    injected: injectedOptions,
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: walletConnectOptions,
+    },
+    portis: {
+      package: Portis,
+      options: portisOptions,
+    },
+    coinbasewallet: {
+      package: CoinbaseWalletSDK,
+      options: coinbaseWalletOptions,
+    },
+  }
+
+  const web3Modal = new Web3Modal({
+    network: chainName === 'Arbitrum' ? 'arbitrum' : 'arbitrum-rinkeby',
+    providerOptions,
+  })
+
+  return <Web3ModalContext.Provider value={{ web3Modal }}>{children}</Web3ModalContext.Provider>
+}
