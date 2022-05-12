42c4e08bdfb0a8652cd247f6fc2efccfb75b01d5
Daniel Izdebski <spidiritsu@gmail.com>
2022-05-12 16:28:51 +0200

    Use Web3Modal from provider in ConnectWalletButton component

--- packages/frontend/src/components/Buttons/ConnectWalletButton.tsx
+++ packages/frontend/src/components/Buttons/ConnectWalletButton.tsx
@@ -1,12 +1,5 @@
-import { CoinbaseWalletSDK } from '@coinbase/wallet-sdk'
-import Portis from '@portis/web3'
 import { useEthers } from '@usedapp/core'
-import WalletConnectProvider from '@walletconnect/web3-provider'
-import { CONFIG } from 'src/config/config'
-import { WALLET_CONNECT_BRIDGE_URL } from 'src/constants/walletConnectBridgeUrl'
-import { useDefaultNetwork } from 'src/hooks/chain/useDefaultNetwork'
-import { useWhichWallet } from 'src/hooks/useWhichWallet'
-import Web3Modal from 'web3modal'
+import { useWeb3Modal } from 'src/hooks/useWeb3Modal'
 
 import { Button, ButtonProps } from './Button'
 
@@ -14,63 +7,11 @@ type ConnectWalletButtonProps = Omit<ButtonProps, 'onClick' | 'children'>
 
 export const ConnectWalletButton = (props: ConnectWalletButtonProps) => {
   const { activate } = useEthers()
-  const { isBraveWallet } = useWhichWallet()
-  const { chainName, chainId, rpcUrl } = useDefaultNetwork()
-
-  const metamaskOptions = {
-    display: {
-      name: isBraveWallet ? 'BraveWallet' : 'MetaMask',
-      description: 'Connect with the provider in your Browser',
-    },
-    package: null,
-  }
-
-  const walletConnectOptions = {
-    bridge: WALLET_CONNECT_BRIDGE_URL,
-    rpc: {
-      [chainId]: rpcUrl,
-    },
-  }
-
-  const portisOptions = {
-    network: {
-      nodeUrl: rpcUrl,
-      chainId: chainId,
-    },
-    id: CONFIG.portisDAppID,
-  }
-
-  const coinbaseWalletOptions = {
-    appName: CONFIG.dappName,
-    rpc: rpcUrl,
-    chainId: chainId,
-    darkMode: false,
-  }
+  const web3modal = useWeb3Modal()
 
   const activateProvider = async () => {
-    const providerOptions = {
-      injected: metamaskOptions,
-      walletconnect: {
-        package: WalletConnectProvider,
-        options: walletConnectOptions,
-      },
-      portis: {
-        package: Portis,
-        options: portisOptions,
-      },
-      coinbasewallet: {
-        package: CoinbaseWalletSDK,
-        options: coinbaseWalletOptions,
-      },
-    }
-
-    const web3Modal = new Web3Modal({
-      network: chainName === 'Arbitrum' ? 'arbitrum' : 'arbitrum-rinkeby',
-      providerOptions,
-    })
-
     try {
-      const provider = await web3Modal.connect()
+      const provider = await web3modal.connect()
       await activate(provider)
     } catch (error: any) {
       console.error(error.message)
