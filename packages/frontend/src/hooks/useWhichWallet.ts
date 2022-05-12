37d5776962b76e173e7e8768806a86310a04b2a1
Daniel Izdebski <spidiritsu@gmail.com>
2022-05-12 14:44:03 +0200

    Add return type to useWhichWallet hook

--- packages/frontend/src/hooks/useWhichWallet.ts
+++ packages/frontend/src/hooks/useWhichWallet.ts
@@ -1,6 +1,12 @@
 import { useEthers } from '@usedapp/core'
 
-export function useWhichWallet() {
+interface WhichWallet {
+  isMetaMask: boolean
+  isWalletConnect: boolean
+  isPortis: boolean
+}
+
+export function useWhichWallet(): WhichWallet {
   const { library } = useEthers()
   const { isMetaMask, bridge: isWalletConnect, isPortis } = (library as any).provider
   return { isMetaMask, isWalletConnect, isPortis }
