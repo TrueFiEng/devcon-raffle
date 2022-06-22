625ce67169baaaf6622edb35d30ae1c48e73f41b
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-06-22 11:39:10 +0200

    Remove console log

--- packages/frontend/src/hooks/useBids.ts
+++ packages/frontend/src/hooks/useBids.ts
@@ -2,7 +2,6 @@ import { useContext } from 'react'
 import { BidsContext } from 'src/providers/Bids/context'
 
 export const useBids = () => {
-  console.log('useBids')
   const { bidsState } = useContext(BidsContext)
   return {
     bids: bidsState.get('bids'),
