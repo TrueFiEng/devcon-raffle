37f2f493d2400eca29008ae2e81d24a92303f302
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-06-24 12:13:51 +0200

    Update test

--- packages/frontend/test/components/BidsList.test.tsx
+++ packages/frontend/test/components/BidsList.test.tsx
@@ -134,7 +134,7 @@ describe('UI: BidsListSection', () => {
     const place = screen.getByText(bid.get('place') + '.')
     // eslint-disable-next-line testing-library/no-node-access
     expect(place.parentElement).toHaveTextContent(
-      `${bid.get('place')}.${formatEtherAmount(bid.get('amount'))} ETH ${shortenEthAddress(bid.get('bidderAddress'))}`
+      `${bid.get('place')}.${formatEtherAmount(bid.get('amount'))} ETH${shortenEthAddress(bid.get('bidderAddress'))}`
     )
   }
   const testHiddenBid = (bid: ImmutableBid) => {
