b13b1fe6152baa357cb60813932fead10cf98925
Maria Rushkova <maria.rushkova@trusttoken.com>
2022-06-27 10:12:46 +0200

    Guess time zone in test

--- packages/frontend/test/utils/formatters/formatEndDate.test.ts
+++ packages/frontend/test/utils/formatters/formatEndDate.test.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from '@ethersproject/bignumber'
+import moment from 'moment-timezone'
 import { formatEndDate } from 'src/utils/formatters'
 
 describe('formatEndDate', () => {
@@ -8,6 +9,9 @@ describe('formatEndDate', () => {
 
   it('Formats the date', () => {
     const timestamp = BigNumber.from(Date.parse('2022-03-08T16:03:27.000Z')).div(1000)
-    expect(formatEndDate(timestamp)).toBe('08.03, 17:03 CET')
+    const testTimeZone = moment.tz.guess()
+    const timeZone = moment.tz.zone(testTimeZone)?.abbr(timestamp.toNumber())
+
+    expect(formatEndDate(timestamp)).toBe(`08.03, 17:03 ${timeZone}`)
   })
 })
