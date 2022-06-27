e81b81d07757c51b602e8d22b9b3b427c4e36c5b
p-sad <sadlik512@gmail.com>
2022-06-27 13:59:23 +0200

    Try another saucy hack

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import * as path from 'path'
+import 'node:buffer'
 
 const production = process.env.NODE_ENV === 'production';
-Buffer.from([])
+// Buffer.from([])
 
 // https://vitejs.dev/config/
 export default defineConfig({
