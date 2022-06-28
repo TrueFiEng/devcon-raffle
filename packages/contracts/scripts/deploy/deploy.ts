3b5e20438aa6a9794124586f18f97859aa84e720
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2022-06-28 13:41:42 +0200

    💨 Adjust scripts end times to match contract requirements (#163)
    * Update times in deployments
    
    * Update increase time script

--- packages/contracts/scripts/deploy/deploy.ts
+++ packages/contracts/scripts/deploy/deploy.ts
@@ -6,6 +6,7 @@ import { auctionRaffleArtifactName, getAuctionRaffleLibraries, multicallArtifact
 
 export const reservePrice = utils.parseEther('0.15')
 export const minBidIncrement = utils.parseEther('0.01')
+export const minStateDuration = 6 * HOUR
 
 export async function deploy(biddingStartTime: number, deployer: SignerWithAddress, hre: HardhatRuntimeEnvironment): Promise<Contract> {
   const auctionRaffle = await deployAuctionRaffle(biddingStartTime, deployer, hre)
@@ -20,8 +21,8 @@ export async function deploy(biddingStartTime: number, deployer: SignerWithAddre
 }
 
 export async function deployAuctionRaffle(biddingStartTime: number, deployer: SignerWithAddress, hre: HardhatRuntimeEnvironment): Promise<Contract> {
-  const biddingEndTime = biddingStartTime + HOUR
-  const claimingEndTime = biddingEndTime + HOUR
+  const biddingEndTime = biddingStartTime + minStateDuration
+  const claimingEndTime = biddingEndTime + minStateDuration
 
   const libraryLink = await deployMaxHeap(deployer, hre)
   const auctionRaffleFactory = await hre.ethers.getContractFactory(auctionRaffleArtifactName, { libraries: libraryLink })
@@ -39,7 +40,7 @@ export async function deployAuctionRaffle(biddingStartTime: number, deployer: Si
 
 export async function deployTestnetAuctionRaffle(biddingStartTime: number, heapLibraryAddress: string, deployer: SignerWithAddress, hre: HardhatRuntimeEnvironment) {
   const biddingEndTime = biddingStartTime + YEAR
-  const claimingEndTime = biddingEndTime + HOUR
+  const claimingEndTime = biddingEndTime + minStateDuration
 
   const auctionRaffleFactory = await hre.ethers.getContractFactory(auctionRaffleArtifactName, { libraries: getAuctionRaffleLibraries(heapLibraryAddress) })
   return auctionRaffleFactory.connect(deployer).deploy(
