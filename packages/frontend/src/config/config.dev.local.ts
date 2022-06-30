84f300e1cf27694c40d9124c7af9da134703fb9b
Daniel Izdebski <spidiritsu@gmail.com>
2022-06-30 12:30:45 +0200

    🩺 Make "Auction & Raffle" name usage consistent across the app (#170)
    * 🩺 Make "Auction & Raffle" name usage consistent across the app
    
    * Update page title
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- packages/frontend/src/config/config.dev.local.ts
+++ packages/frontend/src/config/config.dev.local.ts
@@ -18,6 +18,6 @@ export function getLocalDevConfig(): Config {
     addresses: ADDRESSES,
     backendUrl: 'http://localhost:3001',
     portisDAppID: getStringEnv('PORTIS_DAPP_ID') || '',
-    dappName: 'Devcon 6 Auction+Raffle (LOCAL DEV)',
+    dappName: 'Devcon 6 Auction & Raffle (LOCAL DEV)',
   }
 }
