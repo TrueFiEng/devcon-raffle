10d23e1c9535b6cedf5929993e217b35a86caf21
Daniel Izdebski <spidiritsu@gmail.com>
2022-06-30 12:24:47 +0200

    💰 Change text displayed when contract is in ClaimingClosed state (#169)

--- packages/frontend/src/components/Claim/ClaimingClosed.tsx
+++ packages/frontend/src/components/Claim/ClaimingClosed.tsx
@@ -3,7 +3,7 @@ import { FormWideWrapper } from 'src/components/Form/Form'
 export const ClaimingClosed = () => {
   return (
     <FormWideWrapper>
-      <h2>Claiming is closed ⌛️</h2>
+      <h2>Withdraw period ended ⌛️</h2>
     </FormWideWrapper>
   )
 }
