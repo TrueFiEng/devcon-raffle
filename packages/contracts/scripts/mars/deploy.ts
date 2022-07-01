61b5a46a15bfb3d3c307f91b019c33ab8c62a636
Michał Sieczkowski <michal@ethworks.io>
2022-07-01 12:10:57 +0200

    Set gas limit

--- packages/contracts/scripts/mars/deploy.ts
+++ packages/contracts/scripts/mars/deploy.ts
@@ -2,17 +2,18 @@ import { contract, deploy } from 'ethereum-mars'
 import { AuctionRaffle } from '../../build/artifacts'
 import { config } from './deploymentConfig'
 
-deploy({verify: true}, deployAuctionRaffle)
+deploy({ verify: true }, deployAuctionRaffle)
 
 function deployAuctionRaffle(deployer: string) {
   contract(AuctionRaffle, [
-    deployer, // TODO change to config.initialOwner after we know the address
-    config.biddingStartTime,
-    config.biddingEndTime,
-    config.claimingEndTime,
-    config.auctionWinnersCount,
-    config.raffleWinnersCount,
-    config.reservePrice,
-    config.minBidIncrement,
-  ])
+      deployer, // TODO change to config.initialOwner after we know the address
+      config.biddingStartTime,
+      config.biddingEndTime,
+      config.claimingEndTime,
+      config.auctionWinnersCount,
+      config.raffleWinnersCount,
+      config.reservePrice,
+      config.minBidIncrement,
+    ], { gasLimit: 60_000_000 },
+  )
 }
