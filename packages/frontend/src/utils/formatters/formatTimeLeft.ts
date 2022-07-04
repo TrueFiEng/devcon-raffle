3b7b5c277ec30e3367d8f0f9eb63bc74da151dfd
Michał Sieczkowski <michal@ethworks.io>
2022-07-04 19:14:51 +0200

    Apply review suggestion

--- packages/frontend/src/utils/formatters/formatTimeLeft.ts
+++ packages/frontend/src/utils/formatters/formatTimeLeft.ts
@@ -12,7 +12,7 @@ export function formatTimeLeft(timestamp?: BigNumber, now = Date.now()) {
   const difference = date.diff(now) > 0 ? date.diff(now) : 0
   const duration = moment.duration(difference)
 
-  const days = padZeroes(duration.days())
+  const days = padZeroes(Math.floor(duration.asDays()))
   const hours = padZeroes(duration.hours())
   const minutes = padZeroes(duration.minutes())
 
