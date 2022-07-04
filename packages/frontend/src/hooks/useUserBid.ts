219ad4858b3ebe09c4cf93dddec9e94f89e4f8a3
Daniel Izdebski <spidiritsu@gmail.com>
2022-07-04 13:41:26 +0200

    ⚰️ Hide withdraw button for raffle winners who bid reserve price

--- packages/frontend/src/hooks/useUserBid.ts
+++ packages/frontend/src/hooks/useUserBid.ts
@@ -1,12 +1,13 @@
 import { useCall, useEthers } from '@usedapp/core'
 import { useMemo } from 'react'
 import { WinType } from 'src/components/Claim/WinBid/WinFlowEnum'
-import { useBids } from 'src/hooks'
+import { useBids, useDevconParam } from 'src/hooks'
 import { useDevconContract } from 'src/hooks/contract'
 import { UserBid } from 'src/models/Bid'
 
 export function useUserBid(): UserBid | undefined {
   const { devcon, chainId } = useDevconContract()
+  const { devconValue: reservePrice } = useDevconParam('reservePrice')
   const { account } = useEthers()
   const { bids, bidders } = useBids()
 
@@ -42,9 +43,13 @@ export function useUserBid(): UserBid | undefined {
       }
     }
 
+    const shouldUserBeAbleToWithdraw =
+      reservePrice && bid.winType == WinType.Raffle && bid.claimed === false ? bid.amount.eq(reservePrice) : bid.claimed
+
     return {
       ...bidWithPlace,
       ...bid,
+      claimed: shouldUserBeAbleToWithdraw,
     }
   }, [account, bid, bidders, bids])
 }
