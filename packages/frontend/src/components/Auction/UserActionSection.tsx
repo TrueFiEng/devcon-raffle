06f7d5840e80f4d28b3263331f7831663693889b
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-07-05 11:56:41 +0200

    Handle state loading in UserActionSection

--- packages/frontend/src/components/Auction/UserActionSection.tsx
+++ packages/frontend/src/components/Auction/UserActionSection.tsx
@@ -3,6 +3,8 @@ import { ChainIdWarning, ConnectWalletWarning } from 'src/components/Auction'
 import { BidAwaiting } from 'src/components/Bid/BidAwaiting'
 import { BidFlow } from 'src/components/Bid/BidFlow'
 import { ClaimFlow, ClaimingClosed, ResultsAwaiting } from 'src/components/Claim'
+import { FormSubHeading, FormWideWrapper } from 'src/components/Form'
+import { useContractState } from 'src/hooks'
 import { AuctionState, useAuctionState } from 'src/hooks/useAuctionState'
 import { Colors } from 'src/styles/colors'
 import styled from 'styled-components'
@@ -19,11 +21,25 @@ const UserActions: Record<AuctionState, () => ReactElement> = {
 
 export const UserActionSection = () => {
   const state = useAuctionState()
+  const { isLoading } = useContractState()
+  if (isLoading) {
+    return <StateLoading />
+  }
 
   const Content = UserActions[state]
   return (
     <Wrapper>
       <Content />
+    </Wrapper>
+  )
+}
+
+const StateLoading = () => {
+  return (
+    <Wrapper>
+      <FormWideWrapper>
+        <FormSubHeading>Loading...</FormSubHeading>
+      </FormWideWrapper>
     </Wrapper>
   )
 }
