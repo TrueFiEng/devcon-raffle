509656412f5fcfecd1d8bc099b8c1015850ae536
Michał Sieczkowski <mikee2509@gmail.com>
2022-07-14 19:22:51 +0200

    ❓ Show correct redeem deadline in a Q&A section (#193)
    * ❓ Show correct redeem deadline in a Q&A section
    
    * Fix lint
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- packages/frontend/src/components/Info/Accordion.tsx
+++ packages/frontend/src/components/Info/Accordion.tsx
@@ -165,8 +165,8 @@ export const InfoAccordion = () => {
           <StyledContent>
             <span>
               Your Voucher Code will be available to you for 48 hours after the closing of the Auction & Raffle (
-              <Bold>until July 16 at 08:00 UTC</Bold>). Once you have input your voucher code into our ticket portal,
-              you will go through the standard ticket checkout flow. You can go to{' '}
+              <Bold>until {formatEndDate(redeemTimestamp)}</Bold>). Once you have input your voucher code into our
+              ticket portal, you will go through the standard ticket checkout flow. You can go to{' '}
               <Link href="https://tickets.devcon.org/">our ticket shop here</Link> to redeem your voucher code for a
               Devcon 6 ticket. See you at the conference!
             </span>
